a7500cc825d5fb3892ab28b285ebf5534926c7a9
Milen Ivanov <milensl92@gmail.com>
2021-08-07 17:14:00 +0300

    Added changes in Layout Page

--- ForumSystem.Common/GlobalConstants.cs
+++ ForumSystem.Common/GlobalConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class GlobalConstants
     {
-        public const string SystemName = "ForumSystem";
+        public const string SystemName = "The Fitness Community";
 
         public const string AdministratorRoleName = "Administrator";
     }
