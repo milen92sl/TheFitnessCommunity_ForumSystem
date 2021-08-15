fc9c9b95aee8a016344e4c99e78f3cf7c7f94745
Milen Ivanov <milensl92@gmail.com>
2021-08-15 21:48:09 +0300

    Services-Tests added, added Authorization issue on Index page

---
+++ Tests/ForumSystem.Web.Tests/ServicesTests/CommentsServicesTests.cs
@@ -0,0 +1,212 @@
+﻿namespace ForumSystem.Services.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using ForumSystem.Data;
+    using ForumSystem.Data.Models;
+    using ForumSystem.Data.Repositories;
+    using Microsoft.EntityFrameworkCore;
+    using Xunit;
+
+    public class CommentsServicesTests
+    {
+        [Fact]
+        public async Task CreateMethodShouldAddCorrectNewCommentToDbAndToPost()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var dbContext = new ApplicationDbContext(optionsBuilder.Options);
+
+            var commentsService = new EfDeletableEntityRepository<Comment>(dbContext);
+
+            var comment = new Comment
+            {
+                Content = "testContent",
+                UserId = "Milen92sl",
+            };
+
+            var commentList = new List<Comment>();
+            commentList.Add(comment);
+
+            var postId = Guid.NewGuid().ToString();
+            var post = new Post
+            {
+                Comments = commentList,
+            };
+
+            await dbContext.Posts.AddAsync(post);
+
+            var user = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "Milen92sl",
+            };
+
+            await dbContext.Users.AddAsync(user);
+
+            var commentToAdd = new Comment
+            {
+                Content = "testContent",
+            };
+
+            await commentsService.AddAsync(commentToAdd);
+            await dbContext.SaveChangesAsync();
+
+            Assert.NotNull(dbContext.Comments.FirstOrDefaultAsync());
+            Assert.Equal("testContent", dbContext.Comments.FirstAsync().Result.Content);
+            Assert.Equal("Milen92sl", dbContext.Comments.FirstAsync().Result.UserId);
+        }
+
+        [Fact]
+        public async Task IsInPostMethodShouldReturnTrueIfCommentIsInPost()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
+                 .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var dbContext = new ApplicationDbContext(optionsBuilder.Options);
+
+            var commentsService = new EfDeletableEntityRepository<Comment>(dbContext);
+
+            var user = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "Milen92sl",
+            };
+
+            var post = new Post
+            {
+                UserId = user.Id,
+            };
+
+            var comment = new Comment
+            {
+                Content = "testContent",
+                UserId = user.Id,
+                PostId = post.Id,
+            };
+            await dbContext.Users.AddAsync(user);
+            await dbContext.Posts.AddAsync(post);
+            await dbContext.Comments.AddAsync(comment);
+
+
+            await dbContext.SaveChangesAsync();
+
+
+            var result = commentsService.All().Count(x => x.PostId == comment.PostId);
+
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public async Task CheckAddReplyComment()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
+                 .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var dbContext = new ApplicationDbContext(optionsBuilder.Options);
+
+            var commentsService = new EfDeletableEntityRepository<Comment>(dbContext);
+
+            var user = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "Milen Ivanov",
+            };
+            await dbContext.Users.AddAsync(user);
+
+            var post = new Post
+            {
+                Content = "Post's content",
+                Title = "Posts's title",
+                UserId = user.Id,
+            };
+
+            await dbContext.Posts.AddAsync(post);
+
+            var parentComment = new Comment
+            {
+                Content = "Parent comment content",
+                UserId = user.Id,
+                PostId = post.Id,
+            };
+
+            await dbContext.Comments.AddAsync(parentComment);
+
+            var replyComment = new Comment
+            {
+                ParentId = parentComment.Id,
+                Content = "Reply comment content",
+                UserId = user.Id,
+                PostId = post.Id,
+            };
+
+            await dbContext.Comments.AddAsync(replyComment);
+
+            await dbContext.SaveChangesAsync();
+
+            var replyCommentsCount = dbContext.Comments.Count(x => x.ParentId != null);
+
+            Assert.Equal(1, replyCommentsCount);
+        }
+
+        [Fact]
+        public async Task CheckAddReplyToReplyComment()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
+                 .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var dbContext = new ApplicationDbContext(optionsBuilder.Options);
+
+            var commentsService = new EfDeletableEntityRepository<Comment>(dbContext);
+
+            var user = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "Milen Ivanov",
+            };
+            await dbContext.Users.AddAsync(user);
+
+            var post = new Post
+            {
+                Content = "Post's content",
+                Title = "Posts's title",
+                UserId = user.Id,
+            };
+
+            await dbContext.Posts.AddAsync(post);
+
+            var parentComment = new Comment
+            {
+                Content = "Parent comment content",
+                UserId = user.Id,
+                PostId = post.Id,
+            };
+
+            await dbContext.Comments.AddAsync(parentComment);
+
+            var firstReplyComment = new Comment
+            {
+                ParentId = parentComment.Id,
+                Content = "Reply comment content",
+                UserId = user.Id,
+                PostId = post.Id,
+            };
+            await dbContext.Comments.AddAsync(firstReplyComment);
+
+            var secondReplyComment = new Comment
+            {
+                ParentId = firstReplyComment.Id,
+                Content = "Reply comment content",
+                UserId = user.Id,
+                PostId = post.Id,
+            };
+            await dbContext.Comments.AddAsync(secondReplyComment);
+
+            await dbContext.SaveChangesAsync();
+
+            var replyCommentsCount = dbContext.Comments.Count(x => x.ParentId != null);
+
+            Assert.Equal(2, replyCommentsCount);
+        }
+    }
+}
---
+++ Tests/ForumSystem.Web.Tests/ServicesTests/VotesServicesTests.cs
@@ -0,0 +1,93 @@
+namespace ForumSystem.Services.Tests
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using ForumSystem.Data;
+    using ForumSystem.Data.Models;
+    using ForumSystem.Data.Repositories;
+    using ForumSystem.Services.Data;
+    using Microsoft.EntityFrameworkCore;
+    using Xunit;
+
+    public class VotesServicesTests
+    {
+        [Fact]
+        public async Task TwoDownVotesShouldCountOnce()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var mockRepository = new EfRepository<Vote>(new ApplicationDbContext(options.Options));
+            var service = new VotesService(mockRepository);
+
+            await service.VoteAsync(0, "0a9ec75c-0560-4e5b-94d4-c44429bb7379", true);
+            await service.VoteAsync(0, "0a9ec75c-0560-4e5b-94d4-c44429bb7379", true);
+            var votes = service.GetVotes(0);
+
+            Assert.Equal(1, votes);
+        }
+
+        [Fact]
+        public async Task UpVoteAndDownVoteFromSamePersonShouldNegativeOne()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var mockRepository = new EfRepository<Vote>(new ApplicationDbContext(options.Options));
+            var service = new VotesService(mockRepository);
+
+            await service.VoteAsync(0, "0a9ec75c-0560-4e5b-94d4-c44429bb7379", true);
+            await service.VoteAsync(0, "0a9ec75c-0560-4e5b-94d4-c44429bb7379", false);
+            var votes = service.GetVotes(0);
+
+            Assert.Equal(-1, votes);
+        }
+
+        [Fact]
+        public async Task TwoUpVotesAndOneDownVoteFromThreeUsersShouldBeOne()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var mockRepository = new EfRepository<Vote>(new ApplicationDbContext(options.Options));
+            var service = new VotesService(mockRepository);
+
+            await service.VoteAsync(0, "0a9ec75c-0560-4e5b-94d4-c44429bb7379", true);
+            await service.VoteAsync(0, "175a2793-b1c8-4489-829e-549925137c57", true);
+            await service.VoteAsync(0, "1767ea4e-f3a3-47ac-bdb3-41b71a7f5e7e", false);
+            var votes = service.GetVotes(0);
+
+            Assert.Equal(1, votes);
+        }
+
+        [Fact]
+        public async Task TwoUpVotesAndOneDownVoteFromTwoUsersShouldBeZero()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var mockRepository = new EfRepository<Vote>(new ApplicationDbContext(options.Options));
+            var service = new VotesService(mockRepository);
+
+            await service.VoteAsync(0, "0a9ec75c-0560-4e5b-94d4-c44429bb7379", true);
+            await service.VoteAsync(0, "175a2793-b1c8-4489-829e-549925137c57", true);
+            await service.VoteAsync(0, "0a9ec75c-0560-4e5b-94d4-c44429bb7379", false);
+            var votes = service.GetVotes(0);
+
+            Assert.Equal(0, votes);
+        }
+
+        [Fact]
+        public async Task TwoDownVoteAndOneUpVoteFromTwoUsersShouldBeZero()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var mockRepository = new EfRepository<Vote>(new ApplicationDbContext(options.Options));
+            var service = new VotesService(mockRepository);
+
+            await service.VoteAsync(0, "0a9ec75c-0560-4e5b-94d4-c44429bb7379", false);
+            await service.VoteAsync(0, "175a2793-b1c8-4489-829e-549925137c57", false);
+            await service.VoteAsync(0, "0a9ec75c-0560-4e5b-94d4-c44429bb7379", true);
+            var votes = service.GetVotes(0);
+
+            Assert.Equal(0, votes);
+        }
+    }
+}
